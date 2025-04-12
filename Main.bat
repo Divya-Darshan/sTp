2a3994c0799d24ca5570f0fab33f3aa36a4bbb78
Divya-Darshan <parukrish.darshan20@gamil.com>
2025-04-12 21:20:26 +0530

    Clear all files from GitHub repo

--- Main.bat
+++
@@ -1,8 +0,0 @@
-start Log.txt 
-start H.ahk 
-timeout /t 10 
-start S.ahk 
-start index.bat
-exit
-
-::taskkill /F /IM Notepad.exe
--- main.py
+++
@@ -1,6 +0,0 @@
-from SpeechCapture import speechcapture as sc
-from voiceTo import voiceto as vt
-import os
-
-sc()
-#jsncjsnbc
